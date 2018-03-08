a8341a56792108ed084e78b3cad38602282b393c
luguidong <920430764@qq.com>
2018-03-09 00:34:23 +0800

    'init'

---
+++ server.js
@@ -0,0 +1,16 @@
+const Vue = require('vue')
+const server = require('express')();
+const fs = require('fs');
+const renderer = require('vue-server-renderer').createRenderer();
+const createApp = require('./src/app');
+
+server.get('*', (req, res) => {
+  const context = {url:req.url}
+  const app = createApp(context)
+
+  renderer.renderToString(app,(err,html)=>{
+    
+   res.end(html);
+  })
+})
+server.listen(8080)
